4efffbdeddcaa8ab2ef7ce677ec023137f7bf0c0
xiaofeng630 <1169646434@qq.com>
2026-01-08 13:03:07 +0800

    分词器模块

---
+++ src/llm/eval/tokenizer.py
@@ -0,0 +1,10 @@
+import torch
+
+def text_to_token_ids(text, tokenizer):     
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})     
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) 
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):     
+    flat = token_ids.squeeze(0)   
+    return tokenizer.decode(flat.tolist())
