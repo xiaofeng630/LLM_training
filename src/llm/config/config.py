80ebdb383a66aec61a372728ecc5a625e6354140
xiaofeng630 <1169646434@qq.com>
2026-01-12 11:38:00 +0800

    各种常用配置

---
+++ src/llm/config/config.py
@@ -0,0 +1,6 @@
+MODEL_CONFIGS = {     
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},     
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},     
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},     
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25}, 
+} 
