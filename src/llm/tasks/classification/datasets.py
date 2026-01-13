9d63c570bc7a30c75ee53f471c9541c39dfc8097
xiaofeng630 <1169646434@qq.com>
2026-01-13 16:41:35 +0800

    添加了一个二分类微调数据集类

--- src/llm/tasks/classification/datasets.py
+++ src/llm/tasks/classification/datasets.py
@@ -20,7 +20,7 @@ class SpamDataset(Dataset):
             for encoded_text in self.encoded_texts         
         ]
     
-    def __getitem__(self, index):         
+    def __getitem__(self, index):
         encoded = self.encoded_texts[index]         
         label = self.data.iloc[index]["Label"]         
         return (             
@@ -28,10 +28,10 @@ class SpamDataset(Dataset):
             torch.tensor(label, dtype=torch.long)         
         )
     
-    def __len__(self):         
+    def __len__(self):
         return len(self.data)
 
-    def _longest_encoded_length(self):         
+    def _longest_encoded_length(self):
         max_length = 0
         for encoded_text in self.encoded_texts:             
             encoded_length = len(encoded_text)             
@@ -39,8 +39,70 @@ class SpamDataset(Dataset):
                 max_length = encoded_length         
         return max_length
 
-def create_dataloader_Spam(csv_file_path, batch_size, max_length=None, shuffle=True, drop_last=True, num_workers=0):
-    tokenizer = tiktoken.get_encoding("gpt2")
+class ChnSentiCorpDataset(Dataset):
+    def __init__(
+        self,
+        parquet_file,
+        tokenizer,
+        max_length=None,
+        pad_token_id=0
+    ):
+        """
+        parquet_file: str
+            e.g. train.parquet
+        tokenizer:
+            HuggingFace tokenizer or compatible tokenizer
+        max_length: int or None
+            if None, use longest sequence length in dataset
+        pad_token_id: int
+        """
+        self.df = pd.read_parquet(parquet_file)
+
+        assert "text" in self.df.columns
+        assert "label" in self.df.columns
+
+        # 1. tokenize 所有文本
+        self.encoded_texts = [
+            tokenizer.encode(text)
+            for text in self.df["text"]
+        ]
+
+        # 2. 决定 max_length
+        if max_length is None:
+            self.max_length = self._longest_encoded_length()
+        else:
+            self.max_length = max_length
+            self.encoded_texts = [
+                enc[:self.max_length] for enc in self.encoded_texts
+            ]
+
+        # 3. padding 到 max_length
+        self.encoded_texts = [
+            enc + [pad_token_id] * (self.max_length - len(enc))
+            for enc in self.encoded_texts
+        ]
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        input_ids = torch.tensor(
+            self.encoded_texts[idx], dtype=torch.long
+        )
+        label = torch.tensor(
+            int(self.df.iloc[idx]["label"]), dtype=torch.long
+        )
+
+        return input_ids, label
+
+    def _longest_encoded_length(self):
+        max_len = 0
+        for enc in self.encoded_texts:
+            if len(enc) > max_len:
+                max_len = len(enc)
+        return max_len
+
+def create_dataloader_Spam(csv_file_path, tokenizer, batch_size, max_length=None, shuffle=True, drop_last=True, num_workers=0):
     dataset = SpamDataset(csv_file_path, tokenizer, max_length)
     max_length = dataset.max_length
     dataloader = DataLoader(dataset, batch_size=batch_size, 
@@ -48,7 +110,13 @@ def create_dataloader_Spam(csv_file_path, batch_size, max_length=None, shuffle=T
                             num_workers=num_workers)
     return dataloader
 
-
+def create_dataloader_ChnSentiCorp(parquet_file_path, tokenizer, batch_size, max_length=None, shuffle=True, drop_last=True, num_workers=0):
+    dataset = ChnSentiCorpDataset(parquet_file_path, tokenizer, max_length)
+    max_length = dataset.max_length
+    dataloader = DataLoader(dataset, batch_size=batch_size, 
+                            shuffle=shuffle, drop_last=drop_last, 
+                            num_workers=num_workers)
+    return dataloader
 
 
 
