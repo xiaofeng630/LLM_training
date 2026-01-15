b2e096c38f126b5dd3c47f9907a6085f1e5f3bc9
xiaofeng630 <1169646434@qq.com>
2026-01-15 15:36:11 +0800

    预训练入口文件

--- src/llm/tasks/pretrain/pretraining.py
+++ src/llm/tasks/pretrain/pretraining.py
@@ -1,9 +1,10 @@
 import time
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
 import torch
 from src.llm.model.gpt import GPTModel
+from src.llm.utils.loss_tracker import LossTracker
 from src.llm.config.gpt_configs import GPT_CONFIG_355M
 from src.llm.data.datasets import create_dataloader_jsonls, create_dataloader_bin, create_dataloader_bins
 from src.llm.utils.logger import setup_logger, setup_run_dir
@@ -12,8 +13,9 @@ from src.llm.train.loss import calc_loss_batch, calc_loss_loader
 from src.llm.eval.generate import generate_text_simple_old, generate_text_token, generate_and_print_sample
 import tiktoken
 
-run_dir = setup_run_dir(experiment="pretraining", run_name="gpt124m")
+run_dir = setup_run_dir(experiment="pretraining", run_name="gpt355m")
 logger = setup_logger(name="train", log_file=run_dir / "train.log")
+loss_tracker = LossTracker(run_dir / "samples")
 
 def evaluate_model(model, train_loader, val_loader, device, eval_iter):
     model.eval()     
@@ -38,10 +40,11 @@ def train_model_simple(model, train_loader, val_loader, optimizer, device, num_e
         model.train()
         
         for input_batch, target_batch in train_loader:      
-            optimizer.zero_grad()             
-            loss = calc_loss_batch(
-                input_batch, target_batch, model, device 
-            )
+            optimizer.zero_grad()        
+            with torch.autocast("cuda", dtype=torch.bfloat16):     
+                loss = calc_loss_batch(
+                    input_batch, target_batch, model, device 
+                )
             loss.backward()
             optimizer.step()             
             tokens_seen += input_batch.numel() 
@@ -50,28 +53,31 @@ def train_model_simple(model, train_loader, val_loader, optimizer, device, num_e
             if global_step % eval_freq == 0: 
                 train_loss, val_loss = evaluate_model(                     
                     model, train_loader, val_loader, device, eval_iter
-                )                 
+                )
+                loss_tracker.update_train(global_step, train_loss)
+                loss_tracker.update_val(global_step, val_loss)
                 train_losses.append(train_loss)                 
                 val_losses.append(val_loss)                 
                 track_tokens_seen.append(tokens_seen)  
                 lr = optimizer.param_groups[0]["lr"]
 
-                sample_text = generate_and_print_sample( 
-                    model, tokenizer, device, start_context         
-                )
+                if global_step % (eval_freq * 10) == 0:
+                    sample_text = generate_and_print_sample( 
+                        model, tokenizer, device, start_context         
+                    )
+                    print(f"sample_text: {sample_text}\n")
 
                 logger.info(
                     f"epoch={epoch+1} step={global_step:06d} | "
                     f"train_loss={train_loss:.4f} val_loss={val_loss:.4f} | "
-                    f"tokens_seen={tokens_seen/1e6:.2f}M | lr={lr:.2e}\n"
-                    f"{sample_text}"
+                    f"tokens_seen={tokens_seen/1e6:.2f}M | lr={lr:.2e}"
                 )
+                loss_tracker.plot("loss.png")
 
                 
                 print(f"Ep {epoch+1} (Step {global_step:06d}): "                       
                       f"Train loss {train_loss:.3f}, "                       
-                      f"Val loss {val_loss:.3f}\n"
-                      f"{sample_text}"               
+                      f"Val loss {val_loss:.3f}\n"            
                      )
             
             if global_step % 100000 == 0:
@@ -96,12 +102,14 @@ def train_model_simple(model, train_loader, val_loader, optimizer, device, num_e
 
 if __name__ == "__main__":
     torch.manual_seed(123)
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    GPT_CONFIG_355M["vocab_size"] = tokenizer.max_token_value
     model = GPTModel(GPT_CONFIG_355M)
 
     ## 测试模型是否可以正常推理
     model.eval()
     start_context = "早上出门的时候我才发现忘记带钥匙，只好又回到家里。"
-    tokenizer = tiktoken.get_encoding("cl100k_base")
+    
     token_ids = generate_text_token(
         model=model,     
         idx=text_to_token_ids(start_context, tokenizer),     
@@ -115,8 +123,8 @@ if __name__ == "__main__":
 
     ## 数据加载器
     train_loader = create_dataloader_bins(     
-        "/home/hjzd/lzz/LLM_training/data/CCI3/data_bin/train",     
-        batch_size=16,     
+        "/home/hjzd/lzz/LLM_training/data/pretrain/CCI3/data_bin/train",     
+        batch_size=4,     
         max_length=GPT_CONFIG_355M["context_length"],     
         stride=GPT_CONFIG_355M["context_length"],     
         drop_last=True,     
@@ -125,7 +133,7 @@ if __name__ == "__main__":
     ) 
 
     val_loader = create_dataloader_bins(     
-        "/home/hjzd/lzz/LLM_training/data/CCI3/data_bin/val",     
+        "/home/hjzd/lzz/LLM_training/data/pretrain/CCI3/data_bin/val",     
         batch_size=2,     
         max_length=GPT_CONFIG_355M["context_length"],     
         stride=GPT_CONFIG_355M["context_length"],     
@@ -153,7 +161,7 @@ if __name__ == "__main__":
     num_epochs = 100 
     train_losses, val_losses, tokens_seen = train_model_simple(     
         model, train_loader, val_loader, optimizer, device,     
-        num_epochs=num_epochs, eval_freq=10000, eval_iter=10,     
+        num_epochs=num_epochs, eval_freq=100, eval_iter=10,     
         start_context="早上出门的时候我才发现忘记带钥匙，只好又回到家里。", 
         tokenizer=tokenizer, save_epoch=1
     ) 
