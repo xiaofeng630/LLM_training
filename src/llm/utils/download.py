c0da654c4355a4bf3d24ecd8f64dfe59d99c7005
xiaofeng630 <1169646434@qq.com>
2026-01-16 10:07:45 +0800

    加入官方的指令微调数据集

--- src/llm/utils/download.py
+++ src/llm/utils/download.py
@@ -1,12 +1,14 @@
 import urllib.request 
 import zipfile 
 import os 
+import json
 from pathlib import Path
 url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip" 
 zip_path = "sms_spam_collection.zip" 
 extracted_path = "sms_spam_collection" 
 data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
 
+## 下载官方垃圾邮件分类数据集
 def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):     
     if data_file_path.exists():         
         print(f"{data_file_path} already exists. Skipping download and extraction.")         
@@ -23,7 +25,29 @@ def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     os.rename(original_file_path, data_file_path)
     print(f"File downloaded and saved as {data_file_path}")
 
-download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+## 下载官方指令微调数据集
+def download_and_load_file(file_path, url):     
+    if not os.path.exists(file_path):         
+        with urllib.request.urlopen(url) as response:             
+            text_data = response.read().decode("utf-8")        
+        with open(file_path, "w", encoding="utf-8") as file:             
+            file.write(text_data)    
+    else:          
+        with open(file_path, "r", encoding="utf-8") as file: 
+            text_data = file.read()     
+    with open(file_path, "r") as file:
+        data = json.load(file)     
+    return data
+
+
+if __name__ == "__main__":
+    # download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
+
+    file_path = "instruction-data.json" 
+    url = ("https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch07/01_main-chapter-code/instruction-data.json")
+    data = download_and_load_file(file_path, url) 
+    print("Number of entries:", len(data))
+
 
 
 
